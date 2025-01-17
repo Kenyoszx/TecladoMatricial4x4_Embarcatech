c5b6e91d0f83afe4a537b827a91fad351c0fd4f0
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-17 18:20:20 -0300

    Setar pinos com nivel baixo

--- TecladoMatricial.c
+++ TecladoMatricial.c
@@ -19,7 +19,7 @@
 void inicializar();
 
 int main() {
-    
+
     inicializar();
     
      while (true) {
@@ -33,37 +33,49 @@ void inicializar(){
 
     gpio_init(led_pin_red);
     gpio_set_dir(led_pin_red, GPIO_OUT);
+    gpio_pull_down(led_pin_red);
 
     gpio_init(led_pin_blue);
     gpio_set_dir(led_pin_blue, GPIO_OUT);
+    gpio_pull_down(led_pin_blue);
  
     gpio_init(led_pin_green);
     gpio_set_dir(led_pin_green, GPIO_OUT);
+    gpio_pull_down(led_pin_green);
     
     gpio_init(buzzer_pin);
     gpio_set_dir(buzzer_pin, GPIO_OUT);
+    gpio_pull_down(buzzer_pin);
 
     gpio_init(R1);
     gpio_set_dir(R1, GPIO_IN);
+    gpio_pull_down(R1);
 
     gpio_init(R2);
     gpio_set_dir(R2, GPIO_IN);
+    gpio_pull_down(R2);
 
     gpio_init(R3);
     gpio_set_dir(R3, GPIO_IN);
+    gpio_pull_down(R3);
 
     gpio_init(R4);
     gpio_set_dir(R4, GPIO_IN);
+    gpio_pull_down(R4);
 
     gpio_init(C1);
     gpio_set_dir(C1, GPIO_IN);
+    gpio_pull_down(C1);
 
     gpio_init(C2);
     gpio_set_dir(C2, GPIO_IN);
+    gpio_pull_down(C2);
 
     gpio_init(C3);
     gpio_set_dir(C3, GPIO_IN);
+    gpio_pull_down(C3);
 
     gpio_init(C4);
     gpio_set_dir(C4, GPIO_IN);
+    gpio_pull_down(C4);
 }
