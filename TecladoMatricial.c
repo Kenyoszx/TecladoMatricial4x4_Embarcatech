94486c82a736da44430ef68590754e9a74f5d4ef
João Victor de Souza Silva <joao1452019@gmail.com>
2025-01-18 13:11:28 -0300

    Correção analise do Teclado e Modularização das funções

--- TecladoMatricial.c
+++ TecladoMatricial.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-//definindo funçoes de cada pino
+//definindo funçoes de cada pino, valor de erro e Numero de linhas e colunas
+#define numLinhas 4
+#define numColunas 4
 #define nenhuma -1
 #define led_pin_red 13
 #define led_pin_blue 12
@@ -14,7 +16,7 @@
 #define C1 4
 #define C2 3
 #define C3 2
-#define C4 1
+#define C4 28
 
 //Matriz de Mapeamento formato [colunas] x [linhas]
 char MatrizMapeamento [4][4] = {
@@ -26,94 +28,130 @@ char MatrizMapeamento [4][4] = {
 
 //Protótipos das Funções
 void inicializar();
-int AcharLinha();
-int AcharColuna();
+char AcharDigito();
+void Processar(char);
+
 
 int main() {
-    int linha,coluna;
+    int tecla;
+    stdio_init_all;
     inicializar();
     
      while (true) {
-      coluna = AcharColuna();
-      linha = AcharLinha();
-        
-      
-    }
+        tecla = AcharDigito();
+        Processar(tecla);
 }
-
+}
 void inicializar(){
     // inicialização dos pinos
-    stdio_init_all();
 
     gpio_init(led_pin_red);
     gpio_set_dir(led_pin_red, GPIO_OUT);
-    gpio_pull_down(led_pin_red);
+    gpio_put(led_pin_red,0);
 
     gpio_init(led_pin_blue);
     gpio_set_dir(led_pin_blue, GPIO_OUT);
-    gpio_pull_down(led_pin_blue);
+    gpio_put(led_pin_blue,0);
  
     gpio_init(led_pin_green);
     gpio_set_dir(led_pin_green, GPIO_OUT);
-    gpio_pull_down(led_pin_green);
+    gpio_put(led_pin_green,0);
     
     gpio_init(buzzer_pin);
     gpio_set_dir(buzzer_pin, GPIO_OUT);
-    gpio_pull_down(buzzer_pin);
+    gpio_put(buzzer_pin,0);
 
     gpio_init(R1);
-    gpio_set_dir(R1, GPIO_IN);
-    gpio_pull_down(R1);
+    gpio_set_dir(R1, GPIO_OUT);
+    gpio_put(R1, 1);  // Inicializando as linhas do teclado em nível alto
 
     gpio_init(R2);
-    gpio_set_dir(R2, GPIO_IN);
-    gpio_pull_down(R2);
+    gpio_set_dir(R2, GPIO_OUT);
+    gpio_put(R2, 1);
 
     gpio_init(R3);
-    gpio_set_dir(R3, GPIO_IN);
-    gpio_pull_down(R3);
+    gpio_set_dir(R3, GPIO_OUT);
+    gpio_put(R3, 1);
 
     gpio_init(R4);
-    gpio_set_dir(R4, GPIO_IN);
-    gpio_pull_down(R4);
+    gpio_set_dir(R4, GPIO_OUT);
+    gpio_put(R4, 1);
 
+    // Inicializando os pinos das colunas como entrada com pull up
     gpio_init(C1);
     gpio_set_dir(C1, GPIO_IN);
-    gpio_pull_down(C1);
+    gpio_pull_up(C1);
 
     gpio_init(C2);
     gpio_set_dir(C2, GPIO_IN);
-    gpio_pull_down(C2);
+    gpio_pull_up(C2);
 
     gpio_init(C3);
     gpio_set_dir(C3, GPIO_IN);
-    gpio_pull_down(C3);
+    gpio_pull_up(C3);
 
     gpio_init(C4);
     gpio_set_dir(C4, GPIO_IN);
-    gpio_pull_down(C4);
+    gpio_pull_up(C4);
 }
-int AcharLinha(){
-    int linha = nenhuma;
-    if (gpio_get(R1))
-        linha = 0;
-    else if (gpio_get(R2))
-        linha = 1;
-    else if (gpio_get(R3))
-        linha = 2;
-    else if (gpio_get(R4))
-        linha = 3;
-    return linha;
+char AcharDigito() {
+    for (int linha = 0; linha < 4; linha++) {
+        // Coloca todas as linhas em nível alto
+        gpio_put(R1, 1);
+        gpio_put(R2, 1);
+        gpio_put(R3, 1);
+        gpio_put(R4, 1);
+
+        // Coloca a linha atual em nível baixo
+        switch (linha) {
+            case 0: gpio_put(R1, 0); break;
+            case 1: gpio_put(R2, 0); break;
+            case 2: gpio_put(R3, 0); break;
+            case 3: gpio_put(R4, 0); break;
+        }
+
+        // Verifica as colunas
+        if (!gpio_get(C1)) return MatrizMapeamento[linha][0];
+        if (!gpio_get(C2)) return MatrizMapeamento[linha][1];
+        if (!gpio_get(C3)) return MatrizMapeamento[linha][2];
+        if (!gpio_get(C4)) return MatrizMapeamento[linha][3];
+    }
+
+    // Retorna um caractere nulo caso nenhuma tecla tenha sido pressionada
+    return '\0';
 }
-int AcharColuna(){
-    int coluna = nenhuma;
-    if (gpio_get(C1))
-        coluna = 0;
-    else if (gpio_get(C2))
-        coluna = 1;
-    else if (gpio_get(C3))
-        coluna = 2;
-    else if (gpio_get(C4))
-        coluna = 3;
-    return coluna;
+void Processar(char tecla){
+    if (tecla != '\0'){
+        printf("Tecla %c Pressionada",tecla);
+        switch (tecla){
+        case 'A':
+            gpio_put(led_pin_red,1);
+            break;
+        case 'B':
+            gpio_put(led_pin_blue,1);
+            break;
+        case 'C':
+            gpio_put(led_pin_green,1);
+            break;
+        case '#':
+            gpio_put(buzzer_pin,1);
+            break;
+        case 'D':
+            gpio_put(led_pin_red,1);
+            gpio_put(led_pin_blue,1);
+            gpio_put(led_pin_green,1);
+            break;
+        default:
+            break;
+            }
+        }
+        else
+        {
+            gpio_put(led_pin_red,0);
+            gpio_put(led_pin_blue,0);
+            gpio_put(led_pin_green,0);
+            gpio_put(buzzer_pin,0);
+        }
+        sleep_ms(100);
 }
+
